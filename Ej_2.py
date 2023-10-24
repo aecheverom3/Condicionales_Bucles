dc7367e9c621e517ba7d093806b854a873a1a0e9
aecheverom3 <144794237+aecheverom3@users.noreply.github.com>
2023-10-24 13:48:02 +0200

    Ej_2

---
+++ Ej_2.py
@@ -0,0 +1,7 @@
+número1 = float(input('Escribe un número '))
+número2 = float(input('Escriba otro número '))
+if número2 == 0:
+    print('Math error')
+else:
+    cociente = número1 / número2
+    print(cociente)
